68d3810d657c15bb6359bc451ffcdd59217adcf3
Olivier Barais <olivieR.barais@gmail.com>
2025-03-18 16:45:40 +0100

    update

---
+++ build/serve.js
@@ -0,0 +1,22 @@
+import esbuild from 'esbuild';
+import { createBuildSettings } from './settings.js';
+
+const settings = createBuildSettings({ 
+  sourcemap: true,
+  banner: {
+    js: `new EventSource('/esbuild').addEventListener('change', () => location.reload());`,
+  }
+});
+
+const ctx = await esbuild.context(settings);
+
+await ctx.watch();
+
+const { host, port } = await ctx.serve({
+  port: 5500,
+  servedir: 'www',
+  fallback: "www/index.html"
+});
+
+console.log(`Serving app at http://localhost:${port}.`);
+
